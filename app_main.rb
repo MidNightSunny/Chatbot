378d18db7c43f9d3f8204b62b640013b59548e27
MidNightSunny <52094306+MidNightSunny@users.noreply.github.com>
2019-07-14 19:10:57 +0900

    Update app_main.rb

--- app_main.rb
+++ app_main.rb
@@ -1,11 +1,6 @@
 require 'sinatra'
 require 'line/bot'
 
-# 微小変更部分！確認用。
-get '/' do
-  "Hello world"
-end
-
 def client
   @client ||= Line::Bot::Client.new { |config|
     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
